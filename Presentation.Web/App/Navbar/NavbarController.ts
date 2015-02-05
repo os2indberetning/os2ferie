f01cd5f075a055f6ae8f73f8b46ec1e0fe586dc2
Jimmi Agerskov <jag@it-minds.dk>
2015-02-05 16:15:22 +0100

    Started on DrivingView structure

---
+++ Presentation.Web/App/Navbar/NavbarController.ts
@@ -0,0 +1,19 @@
+﻿/// <reference path="../../scripts/typings/kendo/kendo.all.d.ts" />
+
+module Navbar {
+    'use strict';
+    export interface Scope extends ng.IScope {
+        $state: ng.ui.IState
+    }
+
+    export class Controller {
+
+        constructor(private $scope: Scope) {
+
+            
+
+        }
+    }
+}
+
+Application.AngularApp.Module.controller("NavbarController", Navbar.Controller);
