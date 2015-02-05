0b2f86be560be2b8953e9febbea923ff176b43b8
Jimmi Agerskov <jag@it-minds.dk>
2015-02-05 19:16:31 +0100

    Added datepicker and comboboxes to drivingview

--- Presentation.Web/App/Driving/DrivingController.js
+++ Presentation.Web/App/Driving/DrivingController.js
@@ -1,1 +1,18 @@
+/// <reference path="../../scripts/typings/kendo/kendo.all.d.ts" />
+var Driving;
+(function (Driving) {
+    'use strict';
+    var Controller = (function () {
+        function Controller($scope) {
+            this.$scope = $scope;
+            $scope.date = {
+                start: "month",
+                value: new Date()
+            };
+        }
+        return Controller;
+    })();
+    Driving.Controller = Controller;
+})(Driving || (Driving = {}));
+Application.AngularApp.Module.controller("DrivingController", Driving.Controller);
 //# sourceMappingURL=DrivingController.js.map
