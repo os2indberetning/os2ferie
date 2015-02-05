8f1ae2f9595f7ba07a1954d81d21efbd73810a26
Jimmi Agerskov <jag@it-minds.dk>
2015-02-05 19:16:31 +0100

    Added datepicker and comboboxes to drivingview

--- Presentation.Web/App/Driving/DrivingController.ts
+++ Presentation.Web/App/Driving/DrivingController.ts
@@ -1,1 +1,23 @@
-﻿ 
+﻿ /// <reference path="../../scripts/typings/kendo/kendo.all.d.ts" />
+
+module Driving {
+    'use strict';
+    export interface Scope extends ng.IScope {
+        state: ng.ui.IState;
+        date : any;
+    }
+
+    export class Controller {
+
+        constructor(private $scope: Scope) {
+
+            $scope.date = {
+                start: "month",
+                value: new Date()
+            }
+
+        }
+    }
+}
+
+Application.AngularApp.Module.controller("DrivingController", Driving.Controller);
