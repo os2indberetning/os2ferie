93760c4bcb53021fa3db42aa3d5ceddeef938100
Jimmi Agerskov <jag@it-minds.dk>
2015-02-11 11:34:41 +0100

    Using kendo UI for dropdown boxes

--- Core.ApplicationServices/DriveReportService.cs
+++ Core.ApplicationServices/DriveReportService.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.DomainModel;
+using Core.DomainServices;
 
 namespace Core.ApplicationServices
 {
-    public class DriveReportService(IGenericRepository repository)
-
+    public class DriveReportService
     {
+
     }
 }
