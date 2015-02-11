3415c954396f06bd7c6f20259f353199f89b2d72
Jimmi Agerskov <jag@it-minds.dk>
2015-02-11 11:34:41 +0100

    Using kendo UI for dropdown boxes

--- Presentation.Web/App/Driving/DrivingController.js
+++ Presentation.Web/App/Driving/DrivingController.js
@@ -1,8 +1,22 @@
 ﻿angular.module("application").controller("DrivingController", [
     "$scope", function($scope) {
-        $scope.date = {
-            start: "month",
-            value: new Date()
+
+        $scope.DriveReport = {};
+
+        $scope.DriveReport.Date = new Date();
+
+        $scope.DateOptions = {
+            start: "month"
+        }
+
+  
+        $scope.DrivenKilometers = 33;
+        $scope.TransportAllowance = 33;
+        $scope.RemainingKilometers = 0;
+        $scope.PayoutAmount = 123;
+
+        $scope.print = function() {
+            console.log($scope.DriveReport);
         }
     }
 ]);
