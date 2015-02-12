97ddfc2fbd9e418f240422dcb4fd2052cc8c10fe
Jacob Overgaard Jensen <joj@it-minds.dk>
2015-02-12 13:14:08 +0100

    Removed timestamp from DriveReport. Gets constructed client side now.

--- Core.DomainModel/DriveReport.cs
+++ Core.DomainModel/DriveReport.cs
@@ -18,7 +18,7 @@ namespace Core.DomainModel
         public bool EndsAtHome { get; set; }
         public string Licenseplate { get; set; }
         public string Fullname { get; set; }
-        public string Timestamp { get; set; }
+
 
         public ICollection<DriveReportPoint> DriveReportPoints { get; set; }
     }
