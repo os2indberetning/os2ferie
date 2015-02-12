4310af3a3453c8e6ed2c2f023ccc67038e791635
KSBreuer <ksb@it-minds.dk>
2015-02-12 09:53:30 +0100

    added address services project

---
+++ Infrastructure.AddressServices/Classes/RouteInformation.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Infrastructure.AddressServices.Classes
+{
+    public class RouteInformation
+    {
+        /// <summary>
+        /// String containing route geo points.
+        /// </summary>
+        public string GeoPoints { get; set; }
+        /// <summary>
+        /// Route length.
+        /// </summary>
+        public int Length { get; set; }
+        /// <summary>
+        /// Route duration.
+        /// </summary>
+        public int Duration { get; set; }
+        /// <summary>
+        /// Start street.
+        /// </summary>
+        public string StartStreet { get; set; }
+        /// <summary>
+        /// End street.
+        /// </summary>
+        public string EndStreet { get; set; }
+    }
+}
