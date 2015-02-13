e42d082d3a85d761fc81ff0d353fa466efe7d521
Jacob Overgaard Jensen <joj@it-minds.dk>
2015-02-13 13:37:31 +0100

    Added Jasmine, NUnit and NSubstite. Added backend tests.

---
+++ ApplicationServices.Test/DriveReportServiceTest/DriveReportBaseTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.ApplicationServices;
+using Core.DomainModel;
+using NUnit.Framework;
+
+namespace ApplicationServices.Test.DriveReportServiceTest
+{
+    public class DriveReportBaseTest
+    {
+        protected DriveReport GetDriveReport()
+        {
+            return new DriveReport()
+            {
+                Person = new Person()
+                {
+                    FirstName = "Jacob",
+                    MiddleName = "Overgaard",
+                    LastName = "Jensen"
+                }
+            };
+        }
+
+        protected IQueryable<DriveReport> GetDriveReportAsQueryable()
+        {
+            return new List<DriveReport>()
+            {
+                GetDriveReport()
+            }.AsQueryable();
+        }
+
+        protected DriveReportService GetDriveReportService()
+        {
+            return new DriveReportService();
+        }
+    }
+}
