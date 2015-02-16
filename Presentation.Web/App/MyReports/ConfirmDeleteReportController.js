dbb3efb80397d821eb711637f1dbf4e5d740f6e8
Jacob Overgaard Jensen <joj@it-minds.dk>
2015-02-16 12:08:33 +0100

    Added frontend test file. Added 2 controller files.

---
+++ Presentation.Web/App/MyReports/ConfirmDeleteReportController.js
@@ -0,0 +1,14 @@
+﻿angular.module("application").controller("ConfirmDeleteReportController", [
+    "$scope", "$modalInstance", "itemId", function ($scope, $modalInstance, itemId) {
+
+        $scope.itemId = itemId;
+
+        $scope.confirmDelete = function() {
+            $modalInstance.close($scope.itemId);
+        }
+
+        $scope.cancel = function() {
+            $modalInstance.dismiss('cancel');
+        }
+    }
+]);
