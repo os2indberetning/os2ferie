da66f12da81c9dbb526561dba174608cc89e55d4
Jacob Overgaard Jensen <joj@it-minds.dk>
2015-02-16 12:08:33 +0100

    Added frontend test file. Added 2 controller files.

---
+++ Presentation.Web/App/Services/ReportResource.js
@@ -0,0 +1,5 @@
+﻿angular.module("application").service('Report', ["$resource", function ($resource) {
+    return $resource("/odata/DriveReports(:id)", { id: "@id" }, {
+        "get": { method: "GET", isArray: true }
+    });
+}]);
