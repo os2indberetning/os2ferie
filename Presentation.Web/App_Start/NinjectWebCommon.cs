ccdb993adad57a18cebd20aa4fd5a13224ad5841
Morten Rasmussen <mra@it-minds.dk>
2015-02-19 12:54:53 +0100

    Adds Ninject functionality for web api

--- Presentation.Web/App_Start/NinjectWebCommon.cs
+++ Presentation.Web/App_Start/NinjectWebCommon.cs
@@ -1,54 +1,38 @@
+using System.Reflection;
+using System.Web.Http;
 using Core.DomainServices;
 using Infrastructure.DataAccess;
+using OS2Indberetning.App_Start;
 
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(OS2Indberetning.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(OS2Indberetning.App_Start.NinjectWebCommon), "Stop")]
 
-namespace OS2Indberetning.App_Start
+namespace OS2Indberetning
 {
     using System;
     using System.Web;
-
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 
     using Ninject;
     using Ninject.Web.Common;
 
     public static class NinjectWebCommon 
     {
-        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
-
-        /// <summary>
-        /// Starts the application
-        /// </summary>
-        public static void Start() 
-        {
-            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
-            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
-            bootstrapper.Initialize(CreateKernel);
-        }
-        
-        /// <summary>
-        /// Stops the application.
-        /// </summary>
-        public static void Stop()
-        {
-            bootstrapper.ShutDown();
-        }
-        
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
         /// <returns>The created kernel.</returns>
-        private static IKernel CreateKernel()
+        public static IKernel CreateKernel()
         {
             var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
             try
             {
                 kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
                 kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
                 RegisterServices(kernel);
+
+                // Install our Ninject-based IDependencyResolver into the Web API config
+                GlobalConfiguration.Configuration.DependencyResolver = new NinjectDependencyResolver(kernel);
+
                 return kernel;
             }
             catch
