5cc8582b4bb94a6770097d7a2b3b529f5c9a28fc
Morten Rasmussen <mra@it-minds.dk>
2015-02-19 13:48:23 +0100

    Creates Personal Route and Personal Address controllers and tests for them

---
+++ Presentation.Web.Test/Controllers/PersonalAddresses/PersonalAddressRepositoryMock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Core.DomainModel;
+
+namespace Presentation.Web.Test.Controllers.PersonalAddresses
+{
+    class PersonalAddressRepositoryMock : GenericRepositoryMock<PersonalAddress>
+    {
+        protected override List<PersonalAddress> Seed()
+        {
+            return new List<PersonalAddress>
+            {
+                new PersonalAddress
+                {
+                    Id = 1,
+                    Type = PersonalAddressType.Standard
+                },
+                new PersonalAddress
+                {
+                    Id = 2,
+                    Type = PersonalAddressType.AlternativeHome
+                },
+                new PersonalAddress
+                {
+                    Id = 3,
+                    Type = PersonalAddressType.Work
+                }
+            };
+        }
+    }
+}
