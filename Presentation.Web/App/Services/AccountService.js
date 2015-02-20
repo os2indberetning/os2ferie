c017bf9ade55a50bf50f229100ed548f8545b04f
Jacob Overgaard Jensen <joj@it-minds.dk>
2015-02-20 13:38:25 +0100

    Added ApproveWithDifferentAccount implementation.

--- Presentation.Web/App/Services/AccountService.js
+++
@@ -1,5 +0,0 @@
-﻿angular.module("application").service('Account', ["$resource", function ($resource) {
-    return $resource("/odata/PersonalAddresses(:id)?:query", { id: "@id", query: "@query" }, {
-        "get": { method: "GET", isArray: false }
-    });
-}]);
