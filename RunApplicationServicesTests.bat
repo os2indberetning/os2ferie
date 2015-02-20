2575ebc4cff7fe925f0eae564f1216025ff7b7ef
Morten Rasmussen <mra@it-minds.dk>
2015-02-20 15:13:11 +0100

    Adds open cover to the solution

---
+++ RunApplicationServicesTests.bat
@@ -0,0 +1,5 @@
+REM Newlines are allowed by ending a line with ^
+
+.\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe ^
+ApplicationServices.Test\bin\debug\ApplicationServices.Test.dll ^
+/noshadow /nologo
