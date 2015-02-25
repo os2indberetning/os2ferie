742e69f5c55759738e3a0fd21c2be46d07890641
Morten Rasmussen <mra@it-minds.dk>
2015-02-25 15:28:11 +0100

    Another attempt to fix migrations

--- Infrastructure.DataAccess/Migrations/201502241507287_Update1.cs
+++ Infrastructure.DataAccess/Migrations/201502241507287_Update1.cs
@@ -7,29 +7,12 @@ namespace Infrastructure.DataAccess.Migrations
     {
         public override void Up()
         {
-            CreateTable(
-                "dbo.BankAccounts",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        Number = c.String(nullable: false, unicode: false),
-                        Description = c.String(nullable: false, unicode: false),
-                    })
-                .PrimaryKey(t => t.Id);
-            
-            //AddColumn("dbo.Reports", "Timestamp", c => c.String(unicode: false));
-            //AddColumn("dbo.Reports", "AccountNumber", c => c.String(unicode: false));
-            //AddColumn("dbo.Reports", "KilometerAllowance", c => c.Int());
-            AddColumn("dbo.MobileTokens", "Description", c => c.String(unicode: false));
+            AddColumn("Reports", "KilometerAllowance", c => c.Int());
         }
         
         public override void Down()
         {
-            DropColumn("dbo.MobileTokens", "Description");
-            //DropColumn("dbo.Reports", "KilometerAllowance");
-            //DropColumn("dbo.Reports", "AccountNumber");
-            //DropColumn("dbo.Reports", "Timestamp");
-            DropTable("dbo.BankAccounts");
+            DropColumn("Reports", "KilometerAllowance");
         }
     }
 }
