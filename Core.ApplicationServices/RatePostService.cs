2ade11e4d478b6272e7b5462c504085bc44c185a
Jacob Overgaard Jensen <joj@it-minds.dk>
2015-03-02 12:04:21 +0100

    Added RatePostService Tests.

--- Core.ApplicationServices/RatePostService.cs
+++ Core.ApplicationServices/RatePostService.cs
@@ -14,25 +14,15 @@ namespace Core.ApplicationServices
 {
     public class RatePostService
     {
-        private readonly IRoute<RouteInformation> _route;
-        private readonly IAddressCoordinates _coordinates;
-        private readonly IGenericRepository<DriveReportPoint> _driveReportPointRepository;
-        private readonly IGenericRepository<DriveReport> _driveReportRepository;
-        private readonly IGenericRepository<LicensePlate> _licensePlateRepository;
-        private readonly ReimbursementCalculator _calculator;
 
-        public RatePostService()
+        public bool DeactivateExistingRate(IQueryable<Rate> repo, Rate Rate)
         {
+            if (!repo.AsQueryable().Any(r => r.Year == Rate.Year && r.TypeId == Rate.TypeId && r.Active)) return false;
+
+            var res = repo.AsQueryable().First(r => r.Year == Rate.Year && r.TypeId == Rate.TypeId && r.Active);
+            res.Active = false;
+            return true;
         }
-
-    
-       public void DeactivateExistingRate(IQueryable<Rate> repo, Rate Rate)
-       {
-           if (!repo.AsQueryable().Any(r => r.Year == Rate.Year && r.TypeId == Rate.TypeId && r.Active)) return;
-
-           var res = repo.AsQueryable().First(r => r.Year == Rate.Year && r.TypeId == Rate.TypeId && r.Active);
-           res.Active = false;
-       }
 
 
     }
