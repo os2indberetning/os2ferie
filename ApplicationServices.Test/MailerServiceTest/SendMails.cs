e9472bd3bae72c353c9391ed07879c6ab7342857
Jacob Overgaard Jensen <joj@it-minds.dk>
2015-03-06 13:09:16 +0100

    Added some unwatched files.

---
+++ ApplicationServices.Test/MailerServiceTest/SendMails.cs
@@ -0,0 +1,33 @@
+﻿using Core.ApplicationServices.MailerService.Impl;
+using Core.ApplicationServices.MailerService.Interface;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ApplicationServices.Test.MailerServiceTest
+{
+    [TestFixture]
+    public class SendMails
+    {
+        private MailService uut;
+        private DriveRepoMock driveRepoMock;
+        private SubRepoMock subRepoMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driveRepoMock = new DriveRepoMock();
+            driveRepoMock.ReSeed();
+            subRepoMock = new SubRepoMock();
+            subRepoMock.ReSeed();
+        }
+
+        [Test]
+        public void SendMails_ShouldCall_GetLeadersWithPendingReportsMail()
+        {
+            var senderMock = Substitute.For<IMailSender>();
+            uut = new MailService(driveRepoMock, subRepoMock, senderMock);
+            uut.SendMails();
+            senderMock.ReceivedWithAnyArgs().SendMail("", "", "", "");
+        }
+    }
+}
