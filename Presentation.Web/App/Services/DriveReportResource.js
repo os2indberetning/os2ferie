6514c793f34fe7b9888c2fcaa859ce7396ad6845
jacoboj <joj@it-minds.dk>
2015-03-24 16:12:21 +0100

    Started EINDTO-175

--- Presentation.Web/App/Services/DriveReportResource.js
+++ Presentation.Web/App/Services/DriveReportResource.js
@@ -1,6 +1,14 @@
 ﻿angular.module("application").service('DriveReport', ["$resource", function ($resource) {
     return $resource("/odata/DriveReports(:id)?:query", { id: "@id", query: "@query" }, {
         "get": { method: "GET", isArray: false },
-        "patch": { method: "PATCH" }
+        "patch": { method: "PATCH" },
+        "getLatest": {
+            method: "GET",
+            isArray: false,
+            url: "/odata/DriveReports?$filter=PersonId eq :id &$top=1&$orderby=CreatedDateTimestamp desc",
+            transformResponse: function (data) {
+                return angular.fromJson(data).value[0];
+            }
+        }
     });
 }]);
