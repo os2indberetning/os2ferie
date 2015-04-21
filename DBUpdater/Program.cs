80628b5a8c0c883e6789a49544d192c954a68579
Morten Rasmussen <mra@it-minds.dk>
2015-04-21 15:49:08 +0200

    Updated mysql ef to version 6.9.6. Cleaned up DBUpdater as we decided not to use EF but do the two sql queries directly

--- DBUpdater/Program.cs
+++ DBUpdater/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Migrations.Model;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Infrastructure.DataAccess;
 
 namespace DBUpdater
 {
@@ -10,12 +14,25 @@ namespace DBUpdater
     {
         static void Main(string[] args)
         {
-            using (var context = new ImportDBContext())
-            {
-                var firstOrg = context.Organisation.First();
-                var orgs = context.Organisation.Where(x => x.LOSOrgId > 880000);
-                int xy = 1;
+
+            using (SqlConnection sqlConnection1 = new SqlConnection("data source=706sofd01.intern.syddjurs.dk;initial catalog=MDM;persist security info=True;user id=sofdeindberetning;password=XXXXXXXX")) { 
+                SqlCommand cmd = new SqlCommand();
+                SqlDataReader reader;
+
+                cmd.CommandText = "SELECT * FROM eindberetning.medarbejder";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = sqlConnection1;
+
+                sqlConnection1.Open();
+
+                reader = cmd.ExecuteReader();
+
+                foreach (var x in reader)
+                {
+
+                }
             }
         }
+            
     }
 }
