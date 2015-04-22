d6075a7eea0fd1a214941b8e5e38933845a882be
jacoboj <joj@it-minds.dk>
2015-04-22 14:36:40 +0200

    Small changes

--- DBUpdater/UpdateService.cs
+++ DBUpdater/UpdateService.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Core.DomainModel;
+using Core.DomainServices;
+using Infrastructure.AddressServices.Interfaces;
 
 namespace DBUpdater
 {
     public class UpdateService
     {
+        private readonly CachedAddressLaunderer _cachedLaunderer; 
+        public UpdateService(CachedAddressLaunderer cachedLaunderer)
+        {
+            _cachedLaunderer = cachedLaunderer;
+        }
 
+        public Address GetCleanAddress(Address input)
+        {
+            return _cachedLaunderer.Launder(input);
+        }
     }
 }
