b811b81f80289c1becb7c8554ee7b4c37aa16f77
Morten Rasmussen <mra@it-minds.dk>
2015-06-03 09:13:13 +0200

    Removed old file generator folder, it has been moved to application services

--- FileGenerator/IReportFileWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace FileGenerator
-{
-    public interface IReportFileWriter
-    {
-        void WriteRecordsToFile(ICollection<FileRecord> recordList);
-    }
-}
