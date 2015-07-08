deb2b15ffdac1fde1dc9a851b8fef9a510436364
jacoboj <joj@it-minds.dk>
2015-07-08 15:13:01 +0200

    Changed  PersonSyncService to only sync active people and employments.

--- Infrastructure.DmzSync/Services/Impl/PersonSyncService.cs
+++ Infrastructure.DmzSync/Services/Impl/PersonSyncService.cs
@@ -44,7 +44,7 @@ namespace Infrastructure.DmzSync.Services.Impl
         public void SyncToDmz()
         {
             var i = 0;
-            var personList = _masterPersonRepo.AsQueryable().ToList();
+            var personList = _masterPersonRepo.AsQueryable().Where(x => x.IsActive).ToList();
             var max = personList.Count;
 
             foreach (var person in personList)  
@@ -81,6 +81,7 @@ namespace Infrastructure.DmzSync.Services.Impl
         private void SyncEmployments()
         {
             var i = 0;
+            var currentDateTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
             var personList = _masterPersonRepo.AsQueryable().ToList();
             var max = personList.Count;
 
@@ -95,7 +96,7 @@ namespace Infrastructure.DmzSync.Services.Impl
                 var employments = new List<Employment>();
 
                 // Migrate list of employees as the model is not the same in DMZ and OS2.
-                foreach (var masterEmployment in person.Employments)
+                foreach (var masterEmployment in person.Employments.Where(x => x.EndDateTimestamp > currentDateTimestamp || x.EndDateTimestamp == 0))
                 {
                     var dmzEmployment = new Employment 
                     {
