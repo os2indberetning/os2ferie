683ec72e8734031857793f095f873c2a1f73bc7a
Mikkel Vestergaard <mve@it-minds.dk>
2015-10-01 14:19:16 +0200

    AddressHistoryMigration updated. Not done yet because of lots of unclean addresses.

--- AddressHistoryMigration/DataProvider.cs
+++ AddressHistoryMigration/DataProvider.cs
@@ -8,19 +8,19 @@ using System.Text;
 using System.Threading.Tasks;
 using AddressHistoryMigration.Models;
 
-namespace DBUpdater
+namespace AddressHistoryMigration
 {
     public class DataProvider
     {
         private readonly string _connectionString = ConfigurationManager.ConnectionStrings["DBUpdaterConnection"].ConnectionString;
 
         /// <summary>
-        /// Reads employees from Kommune database and returns them asqueryable.
+        /// Reads employee AddressHistories from Kommune database and returns them asqueryable.
         /// </summary>
         /// <returns></returns>
-        public IQueryable<Employee> GetEmployeesAsQueryable()
+        public IQueryable<AddressHistory> GetAddressHistoriesAsQueryable()
         {
-            var result = new List<Employee>();
+            var result = new List<AddressHistory>();
 
            
 
@@ -28,8 +28,7 @@ namespace DBUpdater
             {
                 var cmd = new SqlCommand
                 {
-                   // CommandText = "SELECT * FROM information_schema.tables",
-                    CommandText = "SELECT * FROM eindberetning.medarbejder",
+                    CommandText = "SELECT * FROM eindberetning.Medarbejder_Temp",
                     CommandType = CommandType.Text,
                     Connection = sqlConnection1
                 };
@@ -40,67 +39,19 @@ namespace DBUpdater
 
                 while (reader.Read())
                 {
-                    var currentRow = new Employee
+                    var currentRow = new AddressHistory
                     {
-                        MaNr = SafeGetInt32(reader, 0),
-                        AnsaettelsesDato = SafeGetDate(reader, 1),
-                        OphoersDato = SafeGetDate(reader, 2),
-                        Fornavn = SafeGetString(reader, 3),
-                        Efternavn = SafeGetString(reader, 4),
-                        ADBrugerNavn = SafeGetString(reader, 5),
-                        Adresse = SafeGetString(reader, 6),
-                        Stednavn = SafeGetString(reader, 7),
-                        PostNr =  SafeGetString(reader, 8) == null ? 0 : int.Parse(SafeGetString(reader, 8)),
-                        By = SafeGetString(reader, 9),
-                        Land = SafeGetString(reader, 10),
-                        Email = SafeGetString(reader, 11),
-                        CPR = SafeGetString(reader, 12),
-                        LOSOrgId = SafeGetInt32(reader, 13),
-                        Leder = reader.GetBoolean(14),
-                        Stillingsbetegnelse = SafeGetString(reader, 15),
-                        Omkostningssted = SafeGetInt64(reader, 16),
-                        AnsatForhold = SafeGetString(reader, 17),
-                        EkstraCiffer = SafeGetInt16(reader, 18)
-                    };
-                    result.Add(currentRow);
-                }
-            }
-            return result.AsQueryable();
-        }
-
-        /// <summary>
-        /// Read Organisations from Kommune database and returns them asQueryably.
-        /// </summary>
-        /// <returns></returns>
-        public IQueryable<Organisation> GetOrganisationsAsQueryable()
-        {
-            var result = new List<Organisation>();
-            using (var sqlConnection1 = new SqlConnection(_connectionString))
-            {
-                var cmd = new SqlCommand
-                {
-                    CommandText = "SELECT * FROM eindberetning.organisation",
-                    CommandType = CommandType.Text,
-                    Connection = sqlConnection1
-                };
-
-                sqlConnection1.Open();
-                var reader = cmd.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    var currentRow = new Organisation
-                    {
-                        LOSOrgId = reader.GetInt32(0),
-                        ParentLosOrgId = SafeGetInt32(reader, 1),
-                        KortNavn = SafeGetString(reader, 2),
+                        AktivFra = SafeGetDate(reader,0) ?? DateTime.Now,
+                        AktivTil = SafeGetDate(reader, 1) ?? DateTime.Now,
+                        MaNr = SafeGetInt32(reader, 2) ?? 0,
                         Navn = SafeGetString(reader, 3),
-                        Gade = SafeGetString(reader, 4),
-                        Stednavn = SafeGetString(reader, 5),
-                        Postnr = SafeGetInt16(reader, 6),
-                        By = SafeGetString(reader, 7),
-                        Omkostningssted = SafeGetInt64(reader, 8),
-                        Level = reader.GetInt32(9)
+                        HjemmeAdresse = SafeGetString(reader, 4),
+                        HjemmePostNr = Convert.ToInt32(SafeGetString(reader, 5)),
+                        HjemmeBy = SafeGetString(reader, 6),
+                        HjemmeLand = SafeGetString(reader, 7),
+                        ArbejdsAdresse =  SafeGetString(reader,8),
+                        ArbejdsPostNr = SafeGetInt16(reader, 9) ?? 0,
+                        ArbejdsBy = SafeGetString(reader, 10),
                     };
                     result.Add(currentRow);
                 }
---
+++ AddressHistoryMigration/Program.cs
@@ -0,0 +1,23 @@
+﻿using AddressHistoryMigration;
+using Core.ApplicationServices;
+using Core.DomainModel;
+using Core.DomainServices;
+using Ninject;
+
+namespace DBUpdater
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var ninjectKernel = NinjectWebKernel.CreateKernel();
+            var updateService = new UpdateService(new DataProvider(), ninjectKernel.Get<IGenericRepository<Employment>>(), ninjectKernel.Get<IGenericRepository<AddressHistory>>(), ninjectKernel.Get<IGenericRepository<PersonalAddress>>(), ninjectKernel.Get<IGenericRepository<WorkAddress>>(), ninjectKernel.Get<IAddressCoordinates>());
+            updateService.MigrateHistories();
+        }
+
+
+
+
+
+    }
+}
