d62dc6140ad2c160cb5117a17a287fab65d49f4f
Jimmi Agerskov <jag@it-minds.dk [credential]>
2015-10-06 16:43:44 +0200

    You are now able to refresh on admin and approve reports. The changes done to the these states should probably also be done to all the other states (CurrentUser might be a promise and therefore not undefined)

--- Presentation.Web/App/application.js
+++ Presentation.Web/App/application.js
@@ -67,7 +67,7 @@ angular.module("application").config(["$stateProvider", "$urlRouterProvider", fu
             templateUrl: "/App/ApproveReports/ApproveReportsView.html",
             resolve: {
                 CurrentUser: ["Person", "$location", "$rootScope", function (Person, $location, $rootScope) {
-                    if ($rootScope.CurrentUser == undefined) {
+                    if ($rootScope.CurrentUser == undefined || ($rootScope.CurrentUser.$$state != undefined && $rootScope.CurrentUser.$$state.status == 0)) {
                         return Person.GetCurrentUser().$promise.then(function (data) {
                             $rootScope.CurrentUser = data;
                             if (!data.IsLeader && !data.IsSubstitute) {
@@ -105,7 +105,8 @@ angular.module("application").config(["$stateProvider", "$urlRouterProvider", fu
             controller: "AdminMenuController",
             resolve: {
                 CurrentUser: ["Person", "$location", "$rootScope", function (Person, $location, $rootScope) {
-                    if ($rootScope.CurrentUser == undefined) {
+                    if ($rootScope.CurrentUser == undefined || ($rootScope.CurrentUser.$$state != undefined && $rootScope.CurrentUser.$$state.status == 0)) {
+
                         return Person.GetCurrentUser().$promise.then(function (data) {
                             $rootScope.CurrentUser = data;
                             if (!data.IsAdmin) {
