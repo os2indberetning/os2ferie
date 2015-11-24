5554a9f258256035628df5bb4b9de5b03bc546c2
JacobOJ <jacobovergaardjensen@gmail.com>
2015-11-24 16:04:15 +0100

    Fixed not being able to search for addresses containing ÆØÅ in new StandardAddress

--- Presentation.Web/App/Admin/Controllers/Address/AddNewAddressController.js
+++ Presentation.Web/App/Admin/Controllers/Address/AddNewAddressController.js
@@ -2,32 +2,7 @@
     "$scope", "$modalInstance", "NotificationService", "StandardAddress", "AddressFormatter", "SmartAdresseSource", "$modal",
     function ($scope, $modalInstance, NotificationService, StandardAddress, AddressFormatter, SmartAdresseSource, $modal) {
 
-        $scope.SmartAddress = {
-            type: "json",
-            minLength: 3,
-            serverFiltering: true,
-            crossDomain: true,
-            transport: {
-                read: {
-                    url: function (item) {
-                        var req = 'https://dawa.aws.dk/adgangsadresser/autocomplete?q=' + item.filter.filters[0].value;
-                        return req;
-                    },
-                    dataType: "jsonp",
-                    data: {
-
-                    }
-                }
-            },
-            schema: {
-                data: function (data) {
-                    return data; // <-- The result is just the data, it doesn't need to be unpacked.
-                }
-            },
-        };
-
-
-
+        $scope.SmartAddress = SmartAdresseSource;
 
         $scope.confirmSave = function () {
             /// <summary>
