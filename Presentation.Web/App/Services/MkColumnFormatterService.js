e52167342aca07856c9550ec1d8d4338d17db5bb
Jacob Jensen <joj@it-minds.dk>
2016-01-08 13:17:58 +0100

    Fixed bug in MkColumnFormatterService.

--- Presentation.Web/App/Services/MkColumnFormatterService.js
+++ Presentation.Web/App/Services/MkColumnFormatterService.js
@@ -6,8 +6,10 @@
                 if (data.StartsAtHome || data.EndsAtHome) {
                     return "<div class='inline margin-left-5' kendo-tooltip k-content=\"'Der er kørt til og/eller fra bopælsadressen,<br>men anvendt Beregnet uden merkørsel'\"><i class='fa fa-minus'></i></div>";
                 }
-
-                return "<i class='fa fa-check'></i>";
+            } else {
+                if (data.StartsAtHome || data.EndsAtHome) {
+                    return "<i class='fa fa-check'></i>";
+                }
             }
             return "";
 
