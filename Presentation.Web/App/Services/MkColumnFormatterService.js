259253dacff7bc18c4d5d035ed2f5858954afc59
JacobOJ <jacobovergaardjensen@gmail.com>
2016-01-11 14:21:07 +0100

    Added tooltip to checkmark in MkColumFormatter.

--- Presentation.Web/App/Services/MkColumnFormatterService.js
+++ Presentation.Web/App/Services/MkColumnFormatterService.js
@@ -7,8 +7,9 @@
                     return "<div class='inline margin-left-5' kendo-tooltip k-content=\"'Der er kørt til og/eller fra bopælsadressen,<br>men anvendt Beregnet uden merkørsel'\"><i class='fa fa-minus'></i></div>";
                 }
             } else {
+
                 if (data.StartsAtHome || data.EndsAtHome) {
-                    return "<i class='fa fa-check'></i>";
+                  return "<div class='inline margin-left-5' kendo-tooltip k-content=\"'Der er kørt til og/eller fra bopælsadressen.<br/> Der er fratrukket merkørsel.'\"><i class='fa fa-check'></i></div>";
                 }
             }
             return "";
