ee64a0a56bf892a9c325c76ef8377ee71c153779
MortenJorgensenItMinds <mjo@it-minds.dk>
2016-02-12 14:26:05 +0100

    Now correctly rounding KM on drivereport view

---
+++ Presentation.Web/App/Filters/FormatKmNumber.js
@@ -0,0 +1,9 @@
+﻿angular.module("application").filter('FormatKmNumber', function () {
+    return function (input) {
+        return (+(Math.round(+(Number(input) + 'e' + 2)) + 'e' + -2)).toFixed(2);
+    };
+}).filter('FormatKmNumberString', function () {
+    return function (input) {
+        return (+(Math.round(+(Number(input) + 'e' + 2)) + 'e' + -2)).toFixed(2).toString().replace(".", ",");
+    };
+})
