7de5ce7bec57cb39f38da86d86313c46651ad117
Jimmi Agerskov <jag@it-minds.dk>
2016-04-14 18:53:33 +0200

    Another missing file.

---
+++ Presentation.Web/App/Drive/ApproveReports/ApproveReportsSettingsController.ts
@@ -0,0 +1,24 @@
+﻿module app.drive {
+    "use strict";
+    
+    import BaseApproveReportsSettingsController = app.core.controllers.BaseApproveReportsSettingsController;
+    import SubstituteType = app.core.models.SubstituteType;
+
+    class DriveApproveReportsSettingsController extends BaseApproveReportsSettingsController {
+
+        static $inject = [
+            "$scope",
+            "Person",
+            "$rootScope",
+            "Autocomplete",
+            "$modal"
+        ];
+
+        constructor(protected $scope, protected Person, protected $rootScope, protected Autocomplete, protected $modal) {
+            super($scope, Person, $rootScope, Autocomplete, $modal, SubstituteType.Drive);
+        }
+
+    }
+
+    angular.module("app.drive").controller("ApproveReportsSettingsController", DriveApproveReportsSettingsController);
+}
